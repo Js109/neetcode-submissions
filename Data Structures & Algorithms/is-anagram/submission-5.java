9a7dd8fbff1d47b374d7c1f762ffade2fd72a4fe
Julian Stier <JulianStier@web.de>
2026-08-11 10:15:00 +0200

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.equals(t)){
+            return true;
+        }
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        // fill map for String s with Character and Counter
+        Map<Character, Integer> charCounter = new HashMap<>();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(!(charCounter.containsKey(c))){
+                charCounter.put(c, 1);
+            } else {
+                int currentCount = charCounter.get(c);
+                charCounter.put(c, currentCount + 1);
+            }
+        }
+
+        // fill map for String t with Character and Counter
+        Map<Character, Integer> charCounterT = new HashMap<>();
+        for (int j = 0; j<t.length(); j++) {
+            char c = t.charAt(j);
+            if(!(charCounterT.containsKey(c))) {
+                charCounterT.put(c, 1);
+            } else {
+                int currentCountT = charCounterT.get(c);
+                charCounterT.put(c, currentCountT + 1);
+            }
+        }
+
+        return charCounter.equals(charCounterT);
+    }
+}
